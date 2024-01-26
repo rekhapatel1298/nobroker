312d5a9c063693f4a2d64bd6428cec689154cfbd
Mom and Dad <rekhapatel1298@gmail.com>
2024-01-26 15:09:15 +0530

    add

---
+++ src/main/java/com/nobroker1/Nobroker1Application.java
@@ -0,0 +1,13 @@
+package com.nobroker1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nobroker1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nobroker1Application.class, args);
+	}
+
+}
